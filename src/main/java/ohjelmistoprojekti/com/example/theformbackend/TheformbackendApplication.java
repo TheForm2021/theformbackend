ba8fb50f4978b210422741f0f33f7aeb454f61fb
Tuukka Grönholm <tmgronholm@gmail.com>
2021-10-28 11:48:06 +0300

    Initial commit

---
+++ src/main/java/ohjelmistoprojekti/com/example/theformbackend/TheformbackendApplication.java
@@ -0,0 +1,15 @@
+package ohjelmistoprojekti.com.example.theformbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheformbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheformbackendApplication.class, args);
+	}
+	
+	// Test comment!!!
+
+}
---
+++ src/test/java/ohjelmistoprojekti/com/example/theformbackend/TheformbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ohjelmistoprojekti.com.example.theformbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheformbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
