1f3707d95e3cb1c492ba6d2c68b793f537597329
Tiia Haverinen <bgo844@myy.haaga-helia.fi>
2021-11-09 15:17:50 +0200

    Answer entity added

---
+++ src/main/java/ohjelmistoprojekti/com/example/theformbackend/domain/Answer.java
@@ -0,0 +1,59 @@
+package ohjelmistoprojekti.com.example.theformbackend.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class Answer {
+	
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long answerId;
+	private String answerText;
+	
+	@ManyToOne
+	@JsonIgnore
+	@JoinColumn(name="questionId")
+	Question question;
+	
+
+	public Answer() {
+		super();
+	}
+	public Answer(String answerText, Question question) {
+		this.answerText=answerText;
+		this.question=question;
+	}
+	public Long getAnswerId() {
+		return answerId;
+	}
+	public void setAnswerId(Long answerId) {
+		this.answerId = answerId;
+	}
+	public String getAnswerText() {
+		return answerText;
+	}
+	public void setAnswerText(String answerText) {
+		this.answerText = answerText;
+	}
+	public Question getQuestion() {
+		return question;
+	}
+	public void setQuestion(Question question) {
+		this.question = question;
+	}
+	@Override
+	public String toString() {
+		return "Answer [answerId=" + answerId + ", answerText=" + answerText + ", question=" + question + "]";
+	}
+	
+	
+
+}
--- src/main/java/ohjelmistoprojekti/com/example/theformbackend/domain/Question.java
+++ src/main/java/ohjelmistoprojekti/com/example/theformbackend/domain/Question.java
@@ -1,5 +1,7 @@
 package ohjelmistoprojekti.com.example.theformbackend.domain;
 
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,8 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-
-
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -26,6 +27,9 @@ public class Question {
 	@JsonIgnore
 	@JoinColumn(name="questionnaryId")
 	private Questionnary questionnary;
+	
+	@OneToMany(cascade=CascadeType.ALL, mappedBy="question")
+	List<Answer> answers;
 	
 	public Question() {
 		super();
